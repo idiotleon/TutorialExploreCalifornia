b038eef4dc468bc96c5bb205e48e90abc158437f
Leon Lu <yanglu@microsoft.com>
2018-06-11 16:20:45 -0700

    demo: Respond to HTTP requests
    module 1.2

--- TutorialExploreCalifornia/Startup.cs
+++ TutorialExploreCalifornia/Startup.cs
@@ -28,9 +28,19 @@ namespace TutorialExploreCalifornia
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.Value.StartsWith("/hello"))
+                {
+                    await context.Response.WriteAsync("Hello ASP.NET Core!");
+                }
+
+                await next();
+            });
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello ASP.NET Core!");
+                await context.Response.WriteAsync("How are you?");
             });
         }
     }
