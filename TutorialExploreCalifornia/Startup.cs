fffe3f9db3951ed38b120488646db85dbb5183a2
Leon Lu <yanglu@microsoft.com>
2018-06-12 09:14:50 -0700

    demo: Serve Static Files
    module 1.4

--- TutorialExploreCalifornia/Startup.cs
+++ TutorialExploreCalifornia/Startup.cs
@@ -28,20 +28,7 @@ namespace TutorialExploreCalifornia
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path.Value.StartsWith("/hello"))
-                {
-                    await context.Response.WriteAsync("Hello ASP.NET Core!");
-                }
-
-                await next();
-            });
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("How are you?");
-            });
+            app.UseFileServer();
         }
     }
 }
