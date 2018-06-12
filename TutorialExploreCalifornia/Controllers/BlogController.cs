08e8a4921524b38dc0f0ca0d961d4830e782e0ef
Leon Lu <yanglu@microsoft.com>
2018-06-12 13:54:02 -0700

    demo: Handle Requests with Controllers
    module 2.2

---
+++ TutorialExploreCalifornia/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TutorialExploreCalifornia.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Blog Posts" };
+        }
+    }
+}
---
+++ TutorialExploreCalifornia/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TutorialExploreCalifornia.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello, ASP.NET Core MVC!";
+        }
+    }
+}
