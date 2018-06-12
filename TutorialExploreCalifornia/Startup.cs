a6537fe845e3115ff051b94eb36a9578b21e6b66
Leon Lu <yanglu@microsoft.com>
2018-06-12 09:29:42 -0700

    demo: Use Custom Configuration
    module 1.6

--- TutorialExploreCalifornia/Startup.cs
+++ TutorialExploreCalifornia/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace TutorialExploreCalifornia
 {
@@ -26,7 +27,11 @@ namespace TutorialExploreCalifornia
             // Error Handling Middleware for Production Env
             app.UseExceptionHandler("/error.html");
 
-            if (env.IsDevelopment())
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            if (configuration.GetValue<bool>("EnableDeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
